28a20c6c6128ad4d9391ce4a62aa3368b02335d0
Angkit Khadka <khadkaankit85@gmail.com>
2024-12-02 21:06:43 -0500

    admin option on loggedin home

--- src/app/components/navigation/Navbar.tsx
+++ src/app/components/navigation/Navbar.tsx
@@ -1,27 +1,45 @@
 import Link from "next/link";
 import styles from "./Navbar.module.css";
 import Searchbar from "./Searchbar";
-export default function () {
+import { getServerSession, Session } from "next-auth";
+import { authOptions } from "@/app/api/(home)/auth/[...nextauth]/route";
+import Image from "next/image";
+export default async function Navbar() {
+  const session: Session | null = await getServerSession(authOptions);
   return (
     <nav className={styles.nav}>
       <Link href="/" className={`${styles.navbutton} text-[#8e4bb1] font-bold`}>
         QuizHub
       </Link>
       <Searchbar />
-      <div className={styles.navbuttonsection}>
-        <Link
-          href="/auth/login"
-          className={`${styles.navbutton} ${styles.loginbutton}`}
-        >
-          Login
+      {!session ? (
+        <div className={styles.navbuttonsection}>
+          <Link
+            href="/auth/login"
+            className={`${styles.navbutton} ${styles.loginbutton}`}
+          >
+            Login
+          </Link>
+          <Link
+            href="/auth/signup"
+            className={`${styles.navbutton} ${styles.signupbutton}`}
+          >
+            Signup
+          </Link>
+        </div>
+      ) : (
+        <Link href="/dashboard" className={styles.profile}>
+          <Image
+            alt="yourpic baby"
+            src={
+              typeof session.user?.image === "string" ? session.user.image : ""
+            }
+            width={40}
+            height={40}
+          />
+          <p className={styles.admin}>Admin</p>
         </Link>
-        <Link
-          href="/auth/signup"
-          className={`${styles.navbutton} ${styles.signupbutton}`}
-        >
-          Signup
-        </Link>
-      </div>
+      )}
     </nav>
   );
 }

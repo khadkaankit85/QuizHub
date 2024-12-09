b5872114da539dc7d4f66be8cf789f2aa2c46ea2
Angkit Khadka <khadkaankit85@gmail.com>
2024-12-09 02:13:49 -0500

    jwt verification added for socket connection

--- server/src/utils/verifyjwt.ts
+++ server/src/utils/verifyjwt.ts
@@ -1,7 +1,22 @@
 import jwt from "jsonwebtoken";
-function verifyJWT(token: string) {
-  jwt.verify(token, "wrong-secret", function (err, decoded) {
-    // err
-    // decoded undefined
-  });
+
+interface payload {
+  name?: string;
+  email: string;
+}
+
+export function verifyJwt(token: string): payload | false {
+  if (!process.env.JWT_SECRET) {
+    return false;
+  }
+  if (!token) {
+    return false;
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) as payload;
+    return decoded;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 }

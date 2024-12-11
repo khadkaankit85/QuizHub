97f25031d988df5544514cc6687cb9e289ffcf53
Angkit Khadka <khadkaankit85@gmail.com>
2024-12-11 12:00:41 -0500

    dynamic route handler

--- src/app/(pages)/play/solo/[quizid]/page.tsx
+++ src/app/(pages)/play/solo/[quizid]/page.tsx
@@ -1,3 +1,26 @@
-export default function SinglePlayer() {
-  return <div> This is single player quiz page</div>;
+"use client";
+//format of url is - /play/solo/[quizid]?question=index
+
+import { useSearchParams } from "next/navigation";
+import { use } from "react";
+
+interface SinglePlayer {
+  params: { quizid: string };
+}
+
+export default function SinglePlayer({ params }: SinglePlayer) {
+  //@ts-ignore
+  const param = use(params) as unknown as SinglePlayer.params;
+  const quizid = param.quizid;
+
+  const searchParams = useSearchParams();
+  const question = searchParams.get("n");
+
+  return (
+    <div>
+      {" "}
+      This is single player quiz page, the id of the quiz is {quizid}, and the
+      question index is {question}
+    </div>
+  );
 }

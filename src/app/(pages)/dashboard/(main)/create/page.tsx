9b7d887d6b779e9d4f59fae3d6858db2f6499dac
Angkit Khadka <khadkaankit85@gmail.com>
2025-01-01 15:23:35 -0500

    profile and create route initiated

---
+++ src/app/(pages)/dashboard/(main)/create/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>This is from where you are gonna create a new quiz</div>;
+};
+
+export default page;
---
+++ src/app/(pages)/dashboard/(main)/profile/page.tsx
@@ -0,0 +1,4 @@
+const page = () => {
+  return <div> this is your profile dude</div>;
+};
+export default page;
